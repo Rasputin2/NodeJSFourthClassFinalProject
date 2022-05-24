6c94febe406fcaa0aa49e20336927366cf7dfe58
Rasputin <jmcdon35@uic.edu>
2022-05-24 11:35:55 -0500

    Express Sessions Part II

--- app.js
+++ app.js
@@ -43,42 +43,30 @@ app.use(session({
   store: new FileStore()
 }));
 
-function auth (req, res, next) {
-    console.log(req.session);
 
-    if (!req.session.user) {
-        var authHeader = req.headers.authorization;
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');                        
-            err.status = 401;
-            next(err);
-            return;
-        }
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var user = auth[0];
-        var pass = auth[1];
-        if (user == 'admin' && pass == 'password') {
-            req.session.user = 'admin';
-            next(); // authorized
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-        }
+// Note that these two mountings occur before authentication
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Authentication function
+function auth (req, res, next) {
+  console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
     }
     else {
-        if (req.session.user === 'admin') {
-            console.log('req.session: ',req.session);
-            next();
-        }
-        else {
-            var err = new Error('You are not authenticated!');
-            err.status = 401;
-            next(err);
-        }
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
     }
+  }
 }
 
 app.use(auth);
@@ -87,8 +75,6 @@ app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // This is where the mounting occurs
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
---
+++ d
@@ -0,0 +1,47 @@
+[33mcommit c5aa55a4d50a2b84f7cabfbbbcd35eb27bd54fd2[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Rasputin <jmcdon35@uic.edu>
+Date:   Tue May 24 08:00:07 2022 -0500
+
+    Express Sessions Part I
+
+[33mcommit 66c76da40f7dcc07f49544a8bbe88d9e3f20be70[m
+Author: Rasputin <jmcdon35@uic.edu>
+Date:   Mon May 23 17:35:22 2022 -0500
+
+    Cookies
+
+[33mcommit 0437cb5347c77e4c48bd88795b5cbda4b3ab333a[m
+Author: Rasputin <jmcdon35@uic.edu>
+Date:   Mon May 23 15:55:08 2022 -0500
+
+    Basic Authentication
+
+[33mcommit 90b5a280f2d429efc06c8ba50d13fb8d9ae13a0f[m
+Author: Rasputin <jmcdon35@uic.edu>
+Date:   Sun May 22 16:58:35 2022 -0500
+
+    Assignment 2
+
+[33mcommit d629b2799c8c9017be556887c9482737c80fe525[m
+Author: Rasputin <jmcdon35@uic.edu>
+Date:   Sun May 22 15:02:50 2022 -0500
+
+    Express REST API with MongoDB and Mongoose Part 2
+
+[33mcommit b100cfd9253570497066940c66904b007972a0f5[m
+Author: Rasputin <jmcdon35@uic.edu>
+Date:   Sun May 22 14:47:40 2022 -0500
+
+    Express REST API with MongoDB and Mongoose Part 1
+
+[33mcommit a763d6a7c3b43184647263a0c96a50145bec2aa9[m
+Author: Rasputin <jmcdon35@uic.edu>
+Date:   Fri May 20 17:37:56 2022 -0500
+
+    Adds .gitignore
+
+[33mcommit 00e87fc1883fb7d4250f79df39822378397dc54d[m
+Author: Rasputin <jmcdon35@uic.edu>
+Date:   Fri May 20 17:36:20 2022 -0500
+
+    Initial Commit
